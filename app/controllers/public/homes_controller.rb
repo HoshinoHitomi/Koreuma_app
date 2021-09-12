666b63b2e54e550582ceccbfe7b797b83bc26041
HoshinoHitomi <hiikochan711@gmail.com>
2021-09-12 16:49:14 +0000

    [Finish]おおまかなレイアウト調整終了

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
-    @food_ranks = Food.find(FavoriteFood.group(:food_id).order('count(food_id) desc').limit(5).pluck(:food_id))
-    @shop_ranks = Shop.find(FavoriteShop.group(:shop_id).order('count(shop_id) desc').limit(5).pluck(:shop_id))
+    @food_ranks = Food.find(FavoriteFood.group(:food_id).order('count(food_id) desc').limit(3).pluck(:food_id))
+    @shop_ranks = Shop.find(FavoriteShop.group(:shop_id).order('count(shop_id) desc').limit(3).pluck(:shop_id))
     @foods = Food.last(4)
     @shops = Shop.last(4)
   end
